c55f645b373e9c3a9dd46de1d4594501231e7ca0
MrMariu5 <marius.sadreika@gmail.com>
2020-10-22 16:36:29 +0300

    Fragments added

--- XamarinLaboratorinis5/XamarinLaboratorinis5/MainActivity.cs
+++ XamarinLaboratorinis5/XamarinLaboratorinis5/MainActivity.cs
@@ -13,7 +13,6 @@ namespace XamarinLaboratorinis5
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)

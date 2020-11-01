998ef0b7d04356a8ca69115b15f09cdebd5b43b5
MrMariu5 <marius.sadreika@gmail.com>
2020-11-01 10:23:47 +0200

    Fragment 1 works

---
+++ XamarinLaboratorinis5/XamarinLaboratorinis5/Fragment1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.RecyclerView.Extensions;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace XamarinLaboratorinis5
+{
+    public class Fragment1 : Android.Support.V4.App.Fragment
+    {
+        public List<string> listViewItems = new List<string>();
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.Fragment1, container, false);
+            
+            ListView listView = view.FindViewById<ListView>(Resource.Id.listView1);
+            
+            listViewItems.Add("Tekstas su A raidėmis");
+            listViewItems.Add("Be ieškomo simbolio");
+            listViewItems.Add("Čia yra ieškomas simbolis");
+            listViewItems.Add("Nieko nebus");
+            listViewItems.Add("Rasi ko ieškai");
+
+            ArrayAdapter<string> adapter = new ArrayAdapter<string>(Context, Android.Resource.Layout.SimpleListItem1, objects: listViewItems.ToArray());
+
+            listView.Adapter = adapter;
+
+            listView.ItemClick += ListView_ItemClick;
+
+            return view;
+        }
+
+        private void ListView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            if(listViewItems[e.Position].Contains("A")
+                || listViewItems[e.Position].Contains("a"))
+            {
+
+            }
+        }
+    }
+}
--- XamarinLaboratorinis5/XamarinLaboratorinis5/MainActivity.cs
+++ XamarinLaboratorinis5/XamarinLaboratorinis5/MainActivity.cs
@@ -1,19 +1,21 @@
 ﻿using Android.App;
 using Android.OS;
-using Android.Support.V7.App;
 using Android.Runtime;
-using Android.Widget;
+using Android.Support.V4.App;
 
 namespace XamarinLaboratorinis5
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : FragmentActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
+
+            var tran = SupportFragmentManager.BeginTransaction();
+            tran.Add(Resource.Id.fragmentContainer, new Fragment1(), "Fragment1");
+            tran.Commit();
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
